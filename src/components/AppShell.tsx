2c326cb11fc2552710479516dae196152b54b9e7
shreeshanarsha-star <shreesha.narsha@gmail.com>
2026-08-22 03:52:58 +0530

    Update AppShell.tsx

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -9,11 +9,11 @@ export default function AppShell({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex w-full min-h-screen">
+    <div className="flex w-full h-screen p-4 gap-4 overflow-hidden">
       <Sidebar />
-      <div className="flex-1 min-w-0 flex flex-col">
+      <div className="flex-1 min-w-0 flex flex-col bg-white rounded-[28px] shadow-soft overflow-hidden">
         <Topbar title={title} />
-        <main className="p-[26px] max-w-[1180px] w-full mx-auto flex-1 flex flex-col">
+        <main className="flex-1 min-h-0 overflow-y-auto p-[26px] max-w-[1180px] w-full mx-auto flex flex-col">
           {children}
         </main>
       </div>
